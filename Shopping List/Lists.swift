e47da1bc36e3fd1dff0ec71d2e2c642452f781fc
Pedro <pedro_oliva@outlook.com.br>
2024-07-30 15:59:06 -0300

    add project

---
+++ Shopping List/Lists.swift
@@ -0,0 +1,55 @@
+//
+//  Lists.swift
+//  Shopping List
+//
+//  Created by Pedro Oliveira on 29/07/24.
+//
+
+import Foundation
+import UIKit
+
+class Lists:UIViewController, UITableViewDelegate, UITableViewDataSource{
+  
+  @IBOutlet weak var tabela: UITableView!
+  var elements = [["name":"Supermercado","itens":"30"], ["name":"Happy hour","itens":"10"],["name":"Almoço de domingo","itens":"5"]]
+  
+  override func viewDidLoad() {
+    tabela.delegate = self
+    tabela.dataSource = self
+  }
+  
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return elements.count
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return "Listas"
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellIdentifier = "ListsCell"
+    let cell:ListsCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as! ListsCell
+    let dicionario = elements[indexPath.row]
+    let name = dicionario["name"]
+    
+    cell.titulo.text = name
+    cell.quantidade.text = dicionario["itens"]
+    
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    return "Toque na linha para selecionar uma lista."
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let dicionario = elements[indexPath.row]
+    print(dicionario)
+    tabela.reloadData()
+  }
+  
+}
---
+++ Shopping List/ListsCell.swift
@@ -0,0 +1,14 @@
+//
+//  ListsCell.swift
+//  Shopping List
+//
+//  Created by Pedro Oliveira on 30/07/24.
+//
+
+import Foundation
+import UIKit
+
+class ListsCell:UITableViewCell {
+  @IBOutlet weak var titulo:UILabel!
+  @IBOutlet weak var quantidade:UILabel!
+}
